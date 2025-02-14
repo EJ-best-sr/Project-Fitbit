0c16be861f10b198f003ff2b4e4a42f7f70fec29
EJ-best-sr <elvira.joanneb@hotmail.com>
2025-02-14 14:48:39 +0100

    Update load_data.py

--- script/part_1/load_data.py
+++ script/part_1/load_data.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import datetime as dt
+
 def load_data(file_path):
     df = pd.read_csv(file_path)
     df['ActivityDate'] = pd.to_datetime(df['ActivityDate'], format='%m/%d/%Y')
