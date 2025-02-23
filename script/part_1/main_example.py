13c5211b9ad77e220ab648744c016a303b218816
thetylernguyen-1 <146976506+thetylernguyen-1@users.noreply.github.com>
2025-02-23 17:10:55 +0100

    Update main_example.py

--- script/part_1/main_example.py
+++ script/part_1/main_example.py
@@ -5,7 +5,8 @@ from plot_workout_frequency_by_day import plot_workout_frequency_by_day
 from plot_regression_line import plot_regression_line
 from plot_calories_per_day import plot_calories_per_day
 from count_unique_users import count_unique_users
-
+from plot_correlation import plot_corr_amongst_all_columns
+from most_and_least_active_weekday import plot_weekday_activity
 # set color palette for default settings
 sns.set_palette("viridis")
 
@@ -27,5 +28,12 @@ plot_regression_line(data, user_id=1503960366)
 
 # plot calories burnt
 plot_calories_per_day(data, user_id=1503960366, start_date='2016-03-25', end_date='2016-04-05')
+
+
+#plot the correlation among all columns
+plot_corr_amongst_all_columns(data)
+
+#plot the weekday activity
+plot_weekday_activity(data)
 
 
