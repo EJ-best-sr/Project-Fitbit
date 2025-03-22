2e9e4f78e3e5952eed961c17a98b72b7f68cbdeb
Anastasiya <anastasiya.piven21@gmail.com>
2025-03-22 13:22:57 +0100

    Change color of total distances plot

--- general/total_distances.py
+++ general/total_distances.py
@@ -6,22 +6,20 @@ import pandas as pd
 
 
 def plot_distances(df):
-    # Aggregate total distance per user
     total_distance_per_user = df.groupby('Id')['TotalDistance'].sum().reset_index()
-    # Convert Id to string (categorical)
     total_distance_per_user['Id'] = total_distance_per_user['Id'].astype(str)
 
-    # Create an interactive bar plot using Plotly
     fig = px.bar(
         total_distance_per_user,
         x='Id',
         y='TotalDistance',
         labels={'Id': 'Users', 'TotalDistance': 'Total Distance'},
-        width=600,  # Increase the width
+        width=800,  
         height=400
     )
+
+    fig.update_traces(marker_color='steelblue')
     
-    # Customize the layout (optional)
     fig.update_layout(
         xaxis_title='', 
         yaxis_title='Total Distance',
