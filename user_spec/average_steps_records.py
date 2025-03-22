fcd88a7d41d2ed41bf5f270c965a4ecd8a937b5e
Anastasiya <anastasiya.piven21@gmail.com>
2025-03-22 09:42:44 +0100

    Add function to ount total steps record for spec user

---
+++ user_spec/average_steps_records.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def count_user_total_steps_records(df_merged, user_id=None, start_date=None, end_date=None):
+    if user_id is not None:
+        df_filtered = df_merged[df_merged['Id'] == user_id].copy()
+    else:
+        df_filtered = df_merged.copy()
+
+    df_filtered.loc[:, 'SleepDate'] = pd.to_datetime(df_filtered['SleepDate'])  
+    if start_date:
+        start_date = pd.to_datetime(start_date)
+        df_filtered = df_filtered[df_filtered['SleepDate'] >= start_date]
+    if end_date:
+        end_date = pd.to_datetime(end_date)
+        df_filtered = df_filtered[df_filtered['SleepDate'] <= end_date]
+        
+    num_records = df_filtered['TotalSteps'].count()
+
+    return num_records
