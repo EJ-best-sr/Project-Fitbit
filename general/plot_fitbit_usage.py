bca1c63ca6b0a7407260822f6dafebe8e4f604b4
Anastasiya <anastasiya.piven21@gmail.com>
2025-03-23 18:49:19 +0100

    Fix the comments

--- general/plot_fitbit_usage.py
+++ general/plot_fitbit_usage.py
@@ -3,18 +3,15 @@ import pandas as pd
 import plotly.graph_objects as go
 
 def plot_fitbit_usage_pie(db_path):
-    # Connect to the database and fetch data
     conn = sqlite3.connect(db_path)
     query = "SELECT Id, ActivityDate FROM daily_activity"
     df = pd.read_sql_query(query, conn)
     conn.close()
 
-    # Convert ActivityDate to datetime and calculate unique days used per user
     df['ActivityDate'] = pd.to_datetime(df['ActivityDate'])
     days_used = df.groupby('Id')['ActivityDate'].nunique().reset_index()
     days_used.columns = ['Id', 'DaysUsed']
 
-    # Categorize days used
     def categorize_days(days):
         if days <= 10:
             return '1–10 Days'
@@ -25,30 +22,25 @@ def plot_fitbit_usage_pie(db_path):
 
     days_used['UsageGroup'] = days_used['DaysUsed'].apply(categorize_days)
 
-    # Count usage groups
     usage_counts = days_used['UsageGroup'].value_counts().reset_index()
     usage_counts.columns = ['Usage Group', 'Count']
 
-    # Define the desired order of categories
     category_order = ['1–10 Days', '11–21 Days', '22–32 Days']
     usage_counts['Usage Group'] = pd.Categorical(usage_counts['Usage Group'], categories=category_order, ordered=True)
     usage_counts = usage_counts.sort_values('Usage Group')
 
-    # Extract labels and values in the desired order
     labels = usage_counts['Usage Group'].tolist()
     values = usage_counts['Count'].tolist()
 
-    # Define colors
     colors = [
         'rgba(32, 178, 170, 0.5)',
         'rgba(0, 128, 128, 0.65)' ,
         'rgba(11, 97, 107, 0.9)'   
     ]
 
-    # Define font sizes
     font_sizes = [40 if label == "11–21 Days" else 16 for label in labels]
 
-    # Create pie chart
+    
     fig = go.Figure(data=[go.Pie(
         labels=labels,
         values=values,
@@ -57,10 +49,9 @@ def plot_fitbit_usage_pie(db_path):
         insidetextorientation="horizontal",
         textfont=dict(color="white", size=font_sizes),
         marker=dict(colors=colors),
-        sort=False  # Disable automatic sorting of the pie chart
+        sort=False  
     )])
 
-    # Update layout
     fig.update_layout(
         showlegend=True,
         template='plotly_white',
@@ -75,7 +66,7 @@ def plot_fitbit_usage_pie(db_path):
             borderwidth=1,
             x=0.80,
             y=0.80,
-            traceorder='normal'  # Ensure the legend follows the order of the data
+            traceorder='normal'  
         )
     )
 
